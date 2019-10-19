86ccce7ecf6895234df7834c5fca9d1e7b37f154
Bogdan Kochurov <b02k5@b02k5.local>
2019-10-19 21:38:59 +0400

    create action set search query

--- src/store/constants/search.js
+++ src/store/constants/search.js
@@ -1,7 +1,9 @@
 import { generateRequestTypes } from "./helpers";
 
 export const GET_SEARCH_DATA = "GET_SEARCH_DATA";
+export const SET_SEARCH_QUERY = "SET_SEARCH_QUERY";
 
 export const searchConstants = {
-  GET_SEARCH_DATA: generateRequestTypes(GET_SEARCH_DATA)
+  GET_SEARCH_DATA: generateRequestTypes(GET_SEARCH_DATA),
+  SET_SEARCH_QUERY: SET_SEARCH_QUERY
 };
--- src/store/reducers/search.js
+++ src/store/reducers/search.js
@@ -1,12 +1,19 @@
 import { searchConstants } from "../constants/search";
 
-export default (state = [], action) => {
+export default (state = {}, action) => {
   switch (action.type) {
     case searchConstants.GET_SEARCH_DATA.SUCCESS: {
-      return [...state];
+      return { ...state, result: action.payload };
+    }
+
+    case searchConstants.SET_SEARCH_QUERY: {
+      return {
+        ...state,
+        query: action.payload
+      };
     }
 
     default:
-      return [...state];
+      return { ...state };
   }
 };
