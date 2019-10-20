954c301434c05b4183bb1f295115b8a23e40f321
Bogdan Kochurov <b02k5@b02k5.local>
2019-10-20 14:57:31 +0400

    create config

---
+++ src/config/index.js
@@ -0,0 +1,10 @@
+export default {
+  API_PATH: "https://api.stackexchange.com/2.2",
+  SEARCH_PATH: "/search",
+  ANSWERS_PATH: "/answers",
+  INTITLE_PARAMS: "intitle=",
+  ORDER_PARAMS: "order=",
+  SORT_PARAMS: "sort=",
+  SITE_PARAMS: "site=",
+  FILTER_PARAMS: "filter="
+};
