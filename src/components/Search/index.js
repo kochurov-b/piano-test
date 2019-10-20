a305bd04b730f462d6afae29982da5ab205f00d2
Bogdan Kochurov <b02k5@b02k5.local>
2019-10-20 15:01:00 +0400

    request processing added for search

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -1,37 +1,48 @@
 import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { Redirect } from "react-router-dom";
 
 import Input from "../FormElements/Input";
 import Button from "../Button";
+import Form from "../Form";
+import { getSearchData, setSearchQuery } from "../../store/actions/search";
 
 import "./styles.css";
-import Form from "../Form";
 
 export default () => {
   const [inputSearch, setInputSearch] = useState("");
+  const [toResultSearch, setToResultSearch] = useState(false);
+  const dispatch = useDispatch();
 
   const handleSubmitForm = event => {
     event.preventDefault();
-    console.log(inputSearch.trim());
+    setToResultSearch(true);
+    dispatch(setSearchQuery(inputSearch));
+    dispatch(getSearchData.request());
   };
 
   return (
-    <div className="search">
-      <h1>Search by Stack Overflow</h1>
-      <Form className="form--search" onSubmit={handleSubmitForm}>
-        <Input
-          type="text"
-          className="input--search"
-          onChange={event => setInputSearch(event.target.value)}
-          placeholder="Enter your question"
-        />
-        <Button
-          type="submit"
-          className="button--search"
-          disabled={inputSearch.trim().length !== 0 ? false : true}
-        >
-          Search
-        </Button>
-      </Form>
-    </div>
+    <>
+      {toResultSearch && <Redirect to="/result" />}
+
+      <div className="search">
+        <h1>Search by Stack Overflow</h1>
+        <Form className="form--search" onSubmit={handleSubmitForm}>
+          <Input
+            type="text"
+            className="input--search"
+            onChange={event => setInputSearch(event.target.value)}
+            placeholder="Enter your question"
+          />
+          <Button
+            type="submit"
+            className="button--search"
+            disabled={inputSearch.trim().length !== 0 ? false : true}
+          >
+            Search
+          </Button>
+        </Form>
+      </div>
+    </>
   );
 };
