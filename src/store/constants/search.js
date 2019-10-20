c926cd1242e91f142d4d5f3737acfd851456dbaf
Bogdan Kochurov <b02k5@b02k5.local>
2019-10-20 20:57:15 +0400

    remove export constants search

--- src/store/constants/search.js
+++ src/store/constants/search.js
@@ -1,7 +1,7 @@
 import { generateRequestTypes } from "./helpers";
 
-export const GET_SEARCH_DATA = "GET_SEARCH_DATA";
-export const SET_SEARCH_QUERY = "SET_SEARCH_QUERY";
+const GET_SEARCH_DATA = "GET_SEARCH_DATA";
+const SET_SEARCH_QUERY = "SET_SEARCH_QUERY";
 
 export const searchConstants = {
   GET_SEARCH_DATA: generateRequestTypes(GET_SEARCH_DATA),
