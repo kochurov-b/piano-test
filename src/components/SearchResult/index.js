7d9658104f916bae3c2a8f4b7fab163c79038355
Bogdan Kochurov <b02k5@b02k5.local>
2019-10-21 12:48:38 +0400

    added BootProcess to SearchResult

--- src/components/SearchResult/index.js
+++ src/components/SearchResult/index.js
@@ -7,37 +7,23 @@ import Table from "../Table";
 import "./styles.css";
 
 export default () => {
-  const { loading, error, result = [] } = useSelector(state => state.search);
+  const { result = [] } = useSelector(state => state.search);
 
   return (
     <>
       <div className="search-result">
         <h2>Search results</h2>
-        {loading ? (
-          <Notification name="Loading ..." className="notification--loading" />
+        {result.length === 0 ? (
+          <Notification
+            name="No results were found for your request!"
+            linkName="Please try again"
+            linkTo="/"
+            className="notification--notice"
+          />
         ) : (
           <div className="search-result__table">
             <Table bodyData={result} fromLocation="search" />
           </div>
-        )}
-
-        {error ? (
-          <Notification
-            name="Oops, something went wrong!"
-            linkName="Please try again"
-            linkTo="/"
-            className="notification--error"
-          />
-        ) : (
-          !loading &&
-          result.length === 0 && (
-            <Notification
-              name="No results were found for your request!"
-              linkName="Please try again"
-              linkTo="/"
-              className="notification--notice"
-            />
-          )
         )}
       </div>
     </>
