e5885336e0ff719d63500ad29fef2e994d2b4875
Bogdan Kochurov <b02k5@b02k5.local>
2019-10-21 10:57:13 +0400

    added params for config

--- src/config/index.js
+++ src/config/index.js
@@ -2,6 +2,8 @@ export default {
   API_PATH: "https://api.stackexchange.com/2.2",
   SEARCH_PATH: "/search",
   ANSWERS_PATH: "/answers",
+  TAGS_PATH: "/tags",
+  FAQ_PATH: "/faq",
   INTITLE_PARAMS: "intitle=",
   ORDER_PARAMS: "order=",
   SORT_PARAMS: "sort=",
