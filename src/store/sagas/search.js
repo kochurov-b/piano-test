9184a82df78532bd9a5b7d219e76a9b181bedaf8
Bogdan Kochurov <b02k5@b02k5.local>
2019-10-21 17:48:05 +0400

    added params to fetch search data to saga

--- src/store/sagas/search.js
+++ src/store/sagas/search.js
@@ -7,11 +7,13 @@ import config from "../../config";
 
 export function* fetchSearchData(action) {
   try {
-    const { items } = yield call(fetchData, {
+    const { query, page } = action.payload;
+    const { items, has_more } = yield call(fetchData, {
       actionPath: config.SEARCH_PATH,
-      query: action.payload
+      query,
+      page
     });
-    yield put(getSearchData.success(items));
+    yield put(getSearchData.success({ items, has_more }));
   } catch (error) {
     yield put(getSearchData.failure(error));
   }
