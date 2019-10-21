8897bf6241a337f06660daff13c8688e40ca05c6
Bogdan Kochurov <b02k5@b02k5.local>
2019-10-21 17:47:15 +0400

    create hooks use infinite scroll

---
+++ src/hooks/index.js
@@ -0,0 +1,33 @@
+import { useEffect, useState } from "react";
+import { debounce } from "../utils/debounce";
+
+export const useInfiniteScroll = hasMore => {
+  const PAGE_INCREMENT = 1;
+  const [loading, setLoading] = useState(false);
+  const [count, setCount] = useState(PAGE_INCREMENT + 1);
+
+  const handleScroll = debounce(() => {
+    if (
+      window.innerHeight + document.documentElement.scrollTop !==
+        document.documentElement.offsetHeight ||
+      loading
+    ) {
+      return false;
+    }
+
+    setLoading(true);
+  }, 500);
+
+  useEffect(() => {
+    if (!loading) return;
+    hasMore && setCount(count + PAGE_INCREMENT);
+    setLoading(false);
+  }, [count, loading, hasMore]);
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  });
+
+  return { count, loading };
+};
