213d08283de82c079363a17e5efba6d28463fa43
Bogdan Kochurov <b02k5@b02k5.local>
2019-10-21 12:52:52 +0400

    remove return from FormElements Input

--- src/components/FormElements/Input/index.js
+++ src/components/FormElements/Input/index.js
@@ -2,13 +2,11 @@ import React from "react";
 
 import "./styles.css";
 
-export default ({ type, onChange, className, placeholder }) => {
-  return (
-    <input
-      type={type}
-      className={className}
-      onChange={onChange}
-      placeholder={placeholder}
-    />
-  );
-};
+export default ({ type, onChange, className, placeholder }) => (
+  <input
+    type={type}
+    className={className}
+    onChange={onChange}
+    placeholder={placeholder}
+  />
+);
