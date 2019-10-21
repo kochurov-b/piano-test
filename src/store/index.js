f37a37ae873eb566f2de75c2b6c336557c994b98
Bogdan Kochurov <b02k5@b02k5.local>
2019-10-21 19:09:34 +0400

    save store to localStorage

--- src/store/index.js
+++ src/store/index.js
@@ -4,15 +4,30 @@ import createSagaMiddleware from "redux-saga";
 
 import rootReducer from "./reducers";
 import rootSaga from "./sagas";
+import { saveState, loadState } from "../utils/localStorage";
+import { debounce } from "../utils/debounce";
 
 const sagaMiddleware = createSagaMiddleware();
+const persistentState = loadState();
 
 const logger = createLogger({
   collapsed: true
 });
 
-export default createStore(
+export const store = createStore(
   rootReducer,
+  persistentState,
   applyMiddleware(logger, sagaMiddleware)
 );
+
+store.subscribe(
+  debounce(() => {
+    saveState({
+      search: store.getState.search,
+      faqs: store.getState.faqs,
+      topQuestions: store.getState.topQuestions
+    });
+  }, 1500)
+);
+
 sagaMiddleware.run(rootSaga);
