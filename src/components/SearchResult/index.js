0346c44b22f9c54b275500c142954331fe29a9b7
Bogdan Kochurov <b02k5@b02k5.local>
2019-10-21 10:58:26 +0400

    added component Table to SearchResult

--- src/components/SearchResult/index.js
+++ src/components/SearchResult/index.js
@@ -1,13 +1,14 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import { Link } from "react-router-dom";
-import Button from "../Button";
+
 import Notification from "../Notification";
+import Table from "../Table";
 
 import "./styles.css";
 
 export default () => {
   const { loading, error, result = [] } = useSelector(state => state.search);
+
   return (
     <>
       <div className="search-result">
@@ -16,53 +17,7 @@ export default () => {
           <Notification name="Loading ..." className="notification--loading" />
         ) : (
           <div className="search-result__table">
-            <table className="table">
-              <thead>
-                <tr className="table__tr">
-                  <th className="tablet__th">Author</th>
-                  <th className="tablet__th">Theme</th>
-                  <th className="tablet__th">Number of responses</th>
-                  <th className="tablet__th">Tags</th>
-                </tr>
-              </thead>
-              <tbody>
-                {result.map(item => {
-                  const {
-                    owner: { display_name },
-                    question_id,
-                    title,
-                    answer_count,
-                    tags
-                  } = item;
-                  return (
-                    <tr key={question_id.toString()} className="table__tr">
-                      <td className="table__td">
-                        <Button className="button--author">
-                          {display_name}
-                        </Button>{" "}
-                      </td>
-                      <td className="table__td">
-                        <Link to={`/answers/${question_id}`}>{title}</Link>
-                      </td>
-                      <td className="table__td">
-                        <Link to={`/answers/${question_id}`}>
-                          {answer_count}
-                        </Link>
-                      </td>
-                      <td className="table__td">
-                        {tags
-                          .map(tag => (
-                            <Button key={tag} className="button--tag">
-                              {tag}
-                            </Button>
-                          ))
-                          .reduce((prev, curr) => [prev, ", ", curr])}
-                      </td>
-                    </tr>
-                  );
-                })}
-              </tbody>
-            </table>
+            <Table bodyData={result} fromLocation="search" />
           </div>
         )}
 
