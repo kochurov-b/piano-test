54abfee0d2bd5cf29a364e9bc579506b0b463bec
Bogdan Kochurov <b02k5@b02k5.local>
2019-10-21 18:56:11 +0400

    change imort store to main

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 
-import store from "./store";
+import { store } from "./store";
 import App from "./components/App";
 import * as serviceWorker from "./serviceWorker";
 import Search from "./components/Search";
