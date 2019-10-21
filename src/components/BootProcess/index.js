5054a2047fa70507e62d47241001f2afa4f1ddb7
Bogdan Kochurov <b02k5@b02k5.local>
2019-10-21 12:47:55 +0400

    added check error for BootProcess

--- src/components/BootProcess/index.js
+++ src/components/BootProcess/index.js
@@ -3,9 +3,19 @@ import { useSelector } from "react-redux";
 
 import Notification from "../Notification";
 
-export default ({ children }) => {
-  const { loading } = useSelector(state => state.faqs);
-  return loading ? (
+export default ({ children, fromLocation }) => {
+  const { loading, error } = useSelector(state =>
+    fromLocation === "search" ? state.search : state.faqs
+  );
+
+  return error ? (
+    <Notification
+      name="Oops, something went wrong!"
+      linkName="Please try again"
+      linkTo="/"
+      className="notification--error"
+    />
+  ) : loading ? (
     <Notification name="Loading ..." className="notification--loading" />
   ) : (
     <div>{children}</div>
