5eadc787325b3b035b2235772d06ff15968a8cf0
Bogdan Kochurov <b02k5@b02k5.local>
2019-10-22 09:54:22 +0400

    create transition for Notification to BootProcess

--- src/components/BootProcess/index.js
+++ src/components/BootProcess/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import { CSSTransition } from "react-transition-group";
 
 import Notification from "../Notification";
 
@@ -12,16 +13,34 @@ export default ({ children, fromLocation }) => {
       : state.topQuestions
   );
 
-  return error ? (
-    <Notification
-      name="Oops, something went wrong!"
-      linkName="Please try again"
-      linkTo="/"
-      className="notification--error"
-    />
-  ) : loading ? (
-    <Notification name="Loading ..." className="notification--loading" />
-  ) : (
-    <div>{children}</div>
+  return (
+    <>
+      <CSSTransition
+        in={error}
+        appear
+        mountOnEnter
+        unmountOnExit
+        timeout={300}
+        classNames="slide-in"
+      >
+        <Notification
+          name="Oops, something went wrong!"
+          linkName="Please try again"
+          linkTo="/"
+          className="notification--error"
+        />
+      </CSSTransition>
+      <CSSTransition
+        in={loading}
+        appear
+        mountOnEnter
+        unmountOnExit
+        timeout={300}
+        classNames="slide-in"
+      >
+        <Notification name="Loading ..." className="notification--loading" />
+      </CSSTransition>
+      {!error && !loading && <div>{children}</div>}
+    </>
   );
 };
